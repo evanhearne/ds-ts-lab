ab32e9bd9a859a41f70a499ff4bfef85a545eb35
Evan (College) <evancollege@Evans-MacBook-Air.local>
2024-09-16 18:12:48 +0100

    Added type-safety to colleague data
     Author:    Evan Hearne <evanhearne02@gmail.com>

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -6,6 +6,15 @@ interface Friend {
     age: number
 }
 
+interface Colleague {
+    name: string;
+    department: string;
+    contact: {
+      email: string;
+      extension: number;
+    };
+  }
+
 const friend1: Friend = { name: "Paul Fleming", phone: "087-12345", age: 25 };
 const friend2: Friend = { name: "Jane Costello", phone: "086--12345", age: 31 };
  
@@ -13,7 +22,7 @@ const friend2: Friend = { name: "Jane Costello", phone: "086--12345", age: 31 };
   console.log(friends[1]);
   
   //   -------------------
-  const colleague1 = {
+  const colleague1: Colleague ={
     name: "Ralph Graham",
     department: "Engineering",
     contact: {
@@ -22,7 +31,7 @@ const friend2: Friend = { name: "Jane Costello", phone: "086--12345", age: 31 };
     },
   };
   
-  const colleague2 = {
+  const colleague2: Colleague = {
     name: "Patti Burke",
     department: "Finance",
     contact: {
@@ -31,7 +40,7 @@ const friend2: Friend = { name: "Jane Costello", phone: "086--12345", age: 31 };
     },
   };
   
-  const colleague3 = {
+  const colleague3: Colleague = {
     name: "Dean Sullivan",
     department: "HR",
     contact: {
